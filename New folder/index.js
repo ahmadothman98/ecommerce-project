7fbb05679bbb3f6dfb87cc0c5c7bf15c5d3f2889
Ahmad Othman <aothman156@gmail.com>
2022-06-13 01:12:05 +0300

    user categories page with linking to db

---
+++ New folder/index.js
@@ -0,0 +1,32 @@
+loadCategories();
+function loadCategories(){
+    axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/get_categories'
+    })
+    .then(function (response){
+        for(let i = 0; i<response.data.categories.length;i++){
+            //appennd categories
+            var cat_name = response.data.categories[i]['name'];
+            var cat_image = response.data.categories[i]['image'];
+            var cat_id = response.data.categories[i]['id'];
+            var category = "<img src=\"./assets/img/index.jpg\" alt=\"cat_name\">\n<h3>"+cat_name+"</h3>"
+            var new_categ = document.createElement('div');
+            new_categ.classList.add('category');
+            new_categ.setAttribute('id' , "cat_"+cat_id);
+            new_categ.innerHTML = category;
+            document.querySelector('#categories_list').append(new_categ);
+            //addCategoryForItems(cat_name,cat_id); // used for selecting category in item addition
+            var cat_element_id = "#cat_"+cat_id;
+            document.querySelector(cat_element_id).addEventListener('click',function(){
+                var category_id =  this.id;
+                category_id = category_id.replace("cat_","")
+                console.log(category_id);
+                
+
+            })
+        console.log(response.data.categories);
+        }
+    })
+
+}
